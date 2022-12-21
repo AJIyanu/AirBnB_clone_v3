9e6a7edb84f6366bfde392606f548d6f05de701e
AJIyanu <aderemijoseph@live.com>
2022-12-21 16:15:56 +0100

    jsonify

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,8 @@ Restful Api
 """
 
 from api.v1.views import app_views
-import json
-
+from flask import jsonify
 
 @app_views.route("/status")
 def ret_status():
-    string = json.dumps({"status": "OK"})
-    return string
+    return jsonify(status="OK")
